8d17d5971c20d929f001b23187d882720d0b3d5c
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-12 11:18:31 +0200

    add navigation

--- client/app/src/main/java/com/example/instaapp_client/view/MainActivity.java
+++ client/app/src/main/java/com/example/instaapp_client/view/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.instaapp_client.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.instaapp_client.R;
+import com.example.instaapp_client.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
