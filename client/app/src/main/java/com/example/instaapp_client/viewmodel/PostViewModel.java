73d89a54ee136fa57273c8e3caab239758dda39e
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-13 16:51:35 +0200

    Add retrofit to gradle

---
+++ client/app/src/main/java/com/example/instaapp_client/viewmodel/PostViewModel.java
@@ -0,0 +1,12 @@
+package com.example.instaapp_client.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.instaapp_client.model.Post;
+
+public class PostViewModel {
+
+    private MutableLiveData<Post> postMutableLiveData;
+
+
+}
