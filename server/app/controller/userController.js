a50e6f8a38ea82d355f5d1d6399aa8197b9ba5af
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-15 10:29:31 +0200

    register work

--- server/app/controller/userController.js
+++ server/app/controller/userController.js
@@ -1,11 +1,14 @@
-const { encryptPass, decryptPass } = require("../util/security");
+const { encryptPass, decryptPass, createToken } = require("../util/security");
 const { users } = require("../model/model");
 
 module.exports = {
   registerUser: async ({ name, lastName, email, password }) => {
+    console.log("info", name, lastName, email, password);
     if (name === "" || lastName === "" || email === "" || password === "")
       return null;
+    console.log("ingkfmnlds");
     const hashedPassword = await encryptPass(password);
+
     const user = {
       id: new Date().getTime(),
       name,
@@ -14,6 +17,9 @@ module.exports = {
       password: hashedPassword,
       confirmed: false,
     };
+    const token = await createToken({ email, name });
     users.push(user);
+
+    return { token, user };
   },
 };
--- server/app/router/userRouter.js
+++ server/app/router/userRouter.js
@@ -3,10 +3,18 @@ const getRequestData = require("../util/getRequestData");
 const tagsRouter = async (request, response) => {
   switch (request.method) {
     case "GET":
-      
       break;
     case "POST":
-      
+      if (request.url === "/api/user/register") {
+        const data = await getRequestData(request);
+        const jsonData = JSON.parse(data);
+        const { token, user } = await userController.registerUser(jsonData);
+        console.log(user, typeof token);
+        if (!!user && !!token) {
+          response.writeHead(200, { "Content-Type": "application/json" });
+          response.end(JSON.stringify({ url: token }));
+        }
+      }
       break;
     case "DELETE":
 
