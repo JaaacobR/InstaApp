f5b6dd54139c0744185a79baf7a6e9c665c1f0ea
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-19 12:42:22 +0200

    adding binding to RegisterActivity

--- client/app/src/main/java/com/example/instaapp_client/view/RegisterActivity.java
+++ client/app/src/main/java/com/example/instaapp_client/view/RegisterActivity.java
@@ -3,14 +3,22 @@ package com.example.instaapp_client.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.instaapp_client.R;
+import com.example.instaapp_client.databinding.ActivityLoginBinding;
+import com.example.instaapp_client.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
     }
 }
