cc78a66bbebcdb445fd9c7b2e83e7a803c320b53
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-19 12:35:47 +0200

    add binding to login activity

--- client/app/src/main/java/com/example/instaapp_client/view/LoginActivity.java
+++ client/app/src/main/java/com/example/instaapp_client/view/LoginActivity.java
@@ -2,15 +2,28 @@ package com.example.instaapp_client.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.instaapp_client.R;
+import com.example.instaapp_client.databinding.ActivityLoginBinding;
+import com.example.instaapp_client.databinding.ActivityMainBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
