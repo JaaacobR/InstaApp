92fa0a599331e65b83cf3dff95b872b4fcfc5ead
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-19 12:05:49 +0200

    Adding login page

---
+++ client/app/src/main/java/com/example/instaapp_client/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.instaapp_client.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.instaapp_client.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
