fc9acbcd6fa2e47cef17e1441da5142fb5709802
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-21 16:30:23 +0200

    implementing UserInterface

--- client/app/src/main/java/com/example/instaapp_client/api/UserInterface.java
+++ client/app/src/main/java/com/example/instaapp_client/api/UserInterface.java
@@ -1,4 +1,17 @@
 package com.example.instaapp_client.api;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface UserInterface {
+    @FormUrlEncoded
+    @POST("/api/user/register")
+    Call<Void> registerUser(
+            @Field("login") String login,
+            @Field("email") String email,
+            @Field("fullName") String fullName,
+            @Field("password") String password
+    );
 }
