b24d341c76e9bf159f77378b45d6da264d526898
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-21 17:57:00 +0200

    implementing fields in user class

--- client/app/src/main/java/com/example/instaapp_client/model/User.java
+++ client/app/src/main/java/com/example/instaapp_client/model/User.java
@@ -1,4 +1,10 @@
 package com.example.instaapp_client.model;
 
 public class User {
+    private String login;
+    private String fullName;
+    private String email;
+    private boolean confirmed;
+
+
 }
