8cec75499f16b2f69ea349e2aac64e56d9e0b080
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-24 12:43:56 +0200

    add UserViewModel

--- client/app/src/main/java/com/example/instaapp_client/requests/LoginRequest.java
+++ client/app/src/main/java/com/example/instaapp_client/requests/LoginRequest.java
@@ -4,7 +4,7 @@ public class LoginRequest {
     private String login;
     private String password;
 
-    public LoginRequest(String login, String fullName, String email, String password) {
+    public LoginRequest(String login, String password) {
         this.login = login;
         this.password = password;
     }
---
+++ client/app/src/main/java/com/example/instaapp_client/viewmodel/UserViewModel.java
@@ -0,0 +1,45 @@
+package com.example.instaapp_client.viewmodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.instaapp_client.model.User;
+import com.example.instaapp_client.requests.LoginRequest;
+import com.example.instaapp_client.requests.RegisterRequest;
+import com.example.instaapp_client.responses.RegisterResponse;
+import com.example.instaapp_client.service.RetrofitService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class UserViewModel extends ViewModel {
+    private MutableLiveData<User> mutableUser;
+
+    public UserViewModel(){
+        this.mutableUser = new MutableLiveData<>();
+    }
+
+    public void loginUser(String login, String password) {
+        Call<User> call = RetrofitService.getUserInterface().loginUser(new LoginRequest(login,password));
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Log.d("token" , response.body().toString());
+                mutableUser.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.d("fail", "fail" + call.toString() + " " + t.getMessage());
+            }
+        });
+    }
+
+    public MutableLiveData<User> getObservedUser(){
+        return mutableUser;
+    }
+}
