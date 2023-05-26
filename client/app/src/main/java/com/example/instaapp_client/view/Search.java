bed301f23e9df371ba910610bc64c558a5b6e409
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-05-26 12:17:46 +0200

    add binding to search

--- client/app/src/main/java/com/example/instaapp_client/view/Search.java
+++ client/app/src/main/java/com/example/instaapp_client/view/Search.java
@@ -9,16 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.instaapp_client.R;
+import com.example.instaapp_client.databinding.FragmentHomeBinding;
+import com.example.instaapp_client.databinding.FragmentSearchBinding;
 
 
 public class Search extends Fragment {
 
-
+    FragmentSearchBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        binding = FragmentSearchBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        return view;
     }
 }
